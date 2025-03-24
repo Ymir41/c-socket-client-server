535d40d109d290704a4c4d4f65c132d61862056b
Ymir 41 <ymir41.artiste@aleeas.com>
2025-03-24 17:58:00 +0100

    added common files (functions for transforming double to and from big endian notation)

---
+++ common.c
@@ -0,0 +1,18 @@
+//
+// Created by janek on 3/24/25.
+//
+
+#include "common.h"
+#include <types.h>
+
+double htond(double num){
+    uint64_t  *num_cast = (uint64_t*) &num;
+    uint64_t be = htobe64(*num_cast);
+    return *(double *) &be;
+}
+
+double ntohd(double num){
+    uint64_t  *num_cast = (uint64_t*) &num;
+    uint64_t be = be64toh(*num_cast);
+    return *(double *) &be;
+}
---
+++ common.h
@@ -0,0 +1,9 @@
+//
+// Created by janek on 3/24/25.
+//
+
+#ifndef COMMON_H
+#define COMMON_H
+double htond(double num);
+double ntohd(double num);
+#endif //COMMON_H
